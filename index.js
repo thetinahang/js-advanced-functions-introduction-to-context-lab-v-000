2e723820eb18177ca70a5cf3216ac54519508953
Tina Hang <mstinahang@gmail.com>
2020-02-22 17:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Your code here
 function createEmployeeRecord(array) {
   employeeRecord = {
-
+    firstName = array[0],
+    familyName = array[1],
+    title = array[2],
+    payPerHour = array[3]
   };
   return employeeRecord;
 }
